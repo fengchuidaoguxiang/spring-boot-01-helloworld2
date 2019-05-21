248f9f86c2c852093c8662007384a3cebc424f9b
fengchuidaoguxiang <33154249+fengchuidaoguxiang@users.noreply.github.com>
2019-05-21 22:06:43 +0800

    Initial commit

---
+++ src/main/java/com/wjx/springboot01helloworld2/SpringBoot01Helloworld2Application.java
@@ -0,0 +1,13 @@
+package com.wjx.springboot01helloworld2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot01Helloworld2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot01Helloworld2Application.class, args);
+    }
+
+}
